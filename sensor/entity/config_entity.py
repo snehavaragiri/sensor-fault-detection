54f1c3bd6dd1b32e8a47e93964addc269f806c9a
SnehaKulkarni <sneha.varagiri@gmail.com>
2022-11-08 22:37:03 +0530

    Model evaluation completed

--- sensor/entity/config_entity.py
+++ sensor/entity/config_entity.py
@@ -75,3 +75,13 @@ class ModelTrainerConfig:
         )
         self.expected_accuracy: float = training_pipeline.MODEL_TRAINER_EXPECTED_SCORE
         self.overfitting_underfitting_threshold = training_pipeline.MODEL_TRAINER_OVER_FIITING_UNDER_FITTING_THRESHOLD
+
+
+class ModelEvaluationConfig:
+
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        self.model_evaluation_dir: str = os.path.join(
+            training_pipeline_config.artifact_dir, training_pipeline.MODEL_EVALUATION_DIR_NAME
+        )
+        self.report_file_path = os.path.join(self.model_evaluation_dir,training_pipeline.MODEL_EVALUATION_REPORT_NAME)
+        self.change_threshold = training_pipeline.MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE
--- sensor/ml/model/estimator.py
+++ sensor/ml/model/estimator.py
@@ -1,3 +1,5 @@
+from sensor.constant.training_pipeline import SAVED_MODEL_DIR,MODEL_FILE_NAME
+import os
 class TargetValueMapping:
     def __init__(self):
         self.neg: int = 0
@@ -31,38 +33,38 @@ class SensorModel:
             raise e
     
 
-# class ModelResolver:
+class ModelResolver:
 
-#     def __init__(self,model_dir=SAVED_MODEL_DIR):
-#         try:
-#             self.model_dir = model_dir
+    def __init__(self,model_dir=SAVED_MODEL_DIR):
+        try:
+            self.model_dir = model_dir
 
-#         except Exception as e:
-#             raise e
+        except Exception as e:
+            raise e
 
-#     def get_best_model_path(self,)->str:
-#         try:
-#             timestamps = list(map(int,os.listdir(self.model_dir)))
-#             latest_timestamp = max(timestamps)
-#             latest_model_path= os.path.join(self.model_dir,f"{latest_timestamp}",MODEL_FILE_NAME)
-#             return latest_model_path
-#         except Exception as e:
-#             raise e
+    def get_best_model_path(self,)->str:
+        try:
+            timestamps = list(map(int,os.listdir(self.model_dir)))
+            latest_timestamp = max(timestamps)
+            latest_model_path= os.path.join(self.model_dir,f"{latest_timestamp}",MODEL_FILE_NAME)
+            return latest_model_path
+        except Exception as e:
+            raise e
 
-#     def is_model_exists(self)->bool:
-#         try:
-#             if not os.path.exists(self.model_dir):
-#                 return False
+    def is_model_exists(self)->bool:
+        try:
+            if not os.path.exists(self.model_dir):
+                return False
 
-#             timestamps = os.listdir(self.model_dir)
-#             if len(timestamps)==0:
-#                 return False
+            timestamps = os.listdir(self.model_dir)
+            if len(timestamps)==0:
+                return False
             
-#             latest_model_path = self.get_best_model_path()
+            latest_model_path = self.get_best_model_path()
 
-#             if not os.path.exists(latest_model_path):
-#                 return False
+            if not os.path.exists(latest_model_path):
+                return False
 
-#             return True
-#         except Exception as e:
-#             raise e
+            return True
+        except Exception as e:
+            raise e
